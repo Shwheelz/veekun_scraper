8d3ce8379a42d86766164e9f7b35373f11508728
Shwheelz <sfbesong@gmail.com>
2015-09-11 16:45:04 -0500

    added Type scraper and organized files

---
+++ Type Class and Scraper/Types.rb
@@ -0,0 +1,343 @@
+class Bug
+  def self.weaknesses
+    ["Fire", "Flying", "Rock"]
+  end
+  def self.resistances
+    ["Fighting", "Grass", "Ground"]
+  end
+  def self.immunities
+    []
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Dark
+  def self.weaknesses
+    ["Bug", "Fairy", "Fighting"]
+  end
+  def self.resistances
+    ["Dark", "Ghost"]
+  end
+  def self.immunities
+    ["Psychic"]
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Dragon
+  def self.weaknesses
+    ["Dragon", "Fairy", "Ice"]
+  end
+  def self.resistances
+    ["Electric", "Fire", "Grass", "Water"]
+  end
+  def self.immunities
+    []
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Electric
+  def self.weaknesses
+    ["Ground"]
+  end
+  def self.resistances
+    ["Electric", "Flying", "Steel"]
+  end
+  def self.immunities
+    []
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Fairy
+  def self.weaknesses
+    ["Poison", "Steel"]
+  end
+  def self.resistances
+    ["Bug", "Dark", "Fighting"]
+  end
+  def self.immunities
+    ["Dragon"]
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Fighting
+  def self.weaknesses
+    ["Fairy", "Flying", "Psychic"]
+  end
+  def self.resistances
+    ["Bug", "Dark", "Rock"]
+  end
+  def self.immunities
+    []
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Fire
+  def self.weaknesses
+    ["Ground", "Rock", "Water"]
+  end
+  def self.resistances
+    ["Bug", "Fairy", "Fire", "Grass", "Ice", "Steel"]
+  end
+  def self.immunities
+    []
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Flying
+  def self.weaknesses
+    ["Electric", "Ice", "Rock"]
+  end
+  def self.resistances
+    ["Bug", "Fighting", "Grass"]
+  end
+  def self.immunities
+    ["Ground"]
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Ghost
+  def self.weaknesses
+    ["Dark", "Ghost"]
+  end
+  def self.resistances
+    ["Bug", "Poison"]
+  end
+  def self.immunities
+    ["Fighting", "Normal"]
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Grass
+  def self.weaknesses
+    ["Bug", "Fire", "Flying", "Ice", "Poison"]
+  end
+  def self.resistances
+    ["Electric", "Grass", "Ground", "Water"]
+  end
+  def self.immunities
+    []
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Ground
+  def self.weaknesses
+    ["Grass", "Ice", "Water"]
+  end
+  def self.resistances
+    ["Poison", "Rock"]
+  end
+  def self.immunities
+    ["Electric"]
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Ice
+  def self.weaknesses
+    ["Fighting", "Fire", "Rock", "Steel"]
+  end
+  def self.resistances
+    ["Ice"]
+  end
+  def self.immunities
+    []
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Normal
+  def self.weaknesses
+    ["Fighting"]
+  end
+  def self.resistances
+    []
+  end
+  def self.immunities
+    ["Ghost"]
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Poison
+  def self.weaknesses
+    ["Ground", "Psychic"]
+  end
+  def self.resistances
+    ["Bug", "Fairy", "Fighting", "Grass", "Poison"]
+  end
+  def self.immunities
+    []
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Psychic
+  def self.weaknesses
+    ["Bug", "Dark", "Ghost"]
+  end
+  def self.resistances
+    ["Fighting", "Psychic"]
+  end
+  def self.immunities
+    []
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Rock
+  def self.weaknesses
+    ["Fighting", "Grass", "Ground", "Steel", "Water"]
+  end
+  def self.resistances
+    ["Fire", "Flying", "Normal", "Poison"]
+  end
+  def self.immunities
+    []
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Steel
+  def self.weaknesses
+    ["Fighting", "Fire", "Ground"]
+  end
+  def self.resistances
+    ["Bug", "Dragon", "Fairy", "Flying", "Grass", "Ice", "Normal", "Psychic", "Rock", "Steel"]
+  end
+  def self.immunities
+    ["Poison"]
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+class Water
+  def self.weaknesses
+    ["Electric", "Grass"]
+  end
+  def self.resistances
+    ["Fire", "Ice", "Steel", "Water"]
+  end
+  def self.immunities
+    []
+  end
+  def self.super_effective
+    ["Dark", "Grass", "Psychic"]
+  end
+  def self.not_very_effective
+    ["Fairy", "Fighting", "Fire", "Flying", "Ghost", "Poison", "Steel"]
+  end
+  def self.doesnt_effect
+    []
+  end
+end
---
+++ Type Class and Scraper/type_scrape.rb
@@ -0,0 +1,78 @@
+require 'watir'
+
+b = Watir::Browser.new :chrome
+b.goto "http://veekun.com/dex/types"
+
+types = %w[Bug Dark Dragon Electric Fairy Fighting Fire Flying Ghost Grass Ground Ice Normal Poison Psychic Rock Steel Water]
+weaknesses = []
+resistances = []
+immunities = []
+super_effective = []
+not_very_effective = []
+doesnt_effect = []
+cell = 0
+
+File.open('Types.rb', 'w') do |f|
+	types.length.times do |i|
+		f.puts "class #{types[i]}"
+
+		# Columns to find Weaknesses and Resistances
+		types.length.times do |j|
+			if b.td(index: cell).text == "½"
+				resistances.push types[j]
+			elsif b.td(index: cell).text == "2"
+				weaknesses.push types[j]
+			elsif b.td(index: cell).text == "0"
+				immunities.push types[j]
+			end
+			cell += 19		
+		end
+
+		# Reset cell for next iteration
+		cell = i + 1
+
+		# Rows for Offense
+		types.length.times do |k|
+			if b.td(index: k).text == "½"
+				not_very_effective.push types[k]
+			elsif b.td(index: k).text == "2"
+				super_effective.push types[k]
+			elsif b.td(index: k).text == "0"
+				doesnt_effect.push types[k]
+			end
+		end
+
+		f.puts "  def self.weaknesses"
+		f.puts "    #{weaknesses}"
+		f.puts "  end"
+
+		f.puts "  def self.resistances"
+		f.puts "    #{resistances}"
+		f.puts "  end"
+
+		f.puts "  def self.immunities"
+		f.puts "    #{immunities}"
+		f.puts "  end"
+
+		f.puts "  def self.super_effective"
+		f.puts "    #{super_effective}"
+		f.puts "  end"
+
+		f.puts "  def self.not_very_effective"
+		f.puts "    #{not_very_effective}"
+		f.puts "  end"
+
+		f.puts "  def self.doesnt_effect"
+		f.puts "    #{doesnt_effect}"
+		f.puts "  end"
+
+		# Reset all arrays
+		weaknesses.clear
+		resistances.clear
+		immunities.clear
+		super_effective.clear
+		not_very_effective.clear
+		doesnt_effect.clear
+	end	
+	f.puts "end"
+end
